4fb25f7bac69e4e1467af801e48402ca0395df40
kevonlin <lin1314101@gmail.com>
2022-08-21 22:40:54 +0800

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * @author kevonlin
+ * @version 1.0
+ * @create 2022/8/21 下午7:00
+ * @description TODO
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * @params []
+     * @return void
+     * @author kevonlin
+     * @create 2022/8/21 下午7:20 
+     * @description TODO
+     **/
+    public void method() {
+
+    }
+}
