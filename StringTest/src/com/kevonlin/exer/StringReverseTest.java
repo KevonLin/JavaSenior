faabb3a194ea7d8431c62ebdbfaf2f8ee1b88253
kevonlin <lin1314101@gmail.com>
2022-08-25 20:12:39 +0800

    修复错误

--- StringTest/src/com/kevonlin/exer/StringReverseTest.java
+++ StringTest/src/com/kevonlin/exer/StringReverseTest.java
@@ -1,41 +1,31 @@
 package com.kevonlin.exer;
 
-import org.junit.Test;
-
 /**
  * @author kevonlin
- * @version 1.0
+ * @version 1.1
  * @create 2022/8/24 下午8:25
  * @description 字符串指定位置和长度进行翻转
  */
 public class StringReverseTest {
     public static void main(String[] args) {
-        String str = "I'm KevonLin";
+        String str = "I'm Kevon";
         System.out.println(str);
-        String res = strReverse(str);
+        String res = strReverse(str, 4, 5);
         System.out.println(res);
     }
 
-    private static String strReverse(String str) {
+    private static String strReverse(String str, int index, int length) {
         char[] chars = str.toCharArray();
-        int start = 0;
-        int end = chars.length;
-        int index;
-        int len;
-        char[] rch;
-
-        return null;
-    }
-
-    private void swap(char[] ch, int a, int b) {
-        char tmp;
-    }
-
-    @Test
-    public void test1() {
-        char a = '1';
-        char b = '2';
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        int start = index;
+        int end = start + length - 1;
+        char[] rch = null;
+        while (start < end) {
+            char tmp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = tmp;
+            start++;
+            end--;
+        }
+        return new String(chars);
     }
 }
