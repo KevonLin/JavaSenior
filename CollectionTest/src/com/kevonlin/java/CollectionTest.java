5388654d615e1ae7496508877f3f63cf95d2414a
kevonlin <lin1314101@gmail.com>
2022-08-27 15:14:52 +0800

    今天的学习

---
+++ CollectionTest/src/com/kevonlin/java/CollectionTest.java
@@ -0,0 +1,14 @@
+package com.kevonlin.java;
+
+/**
+ * @author kevonlin
+ * @version 1.0
+ * @create 2022/8/27 上午11:58
+ * @description TODO
+ */
+public class CollectionTest {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(str.charAt(1));
+    }
+}
